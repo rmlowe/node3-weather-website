127fb7b4dde062e6718c178d87456c18e9957d10
Robert Lowe <robertmlowe@rmlowe.com>
2020-05-09 01:07:05 +0800

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,25 +1,17 @@
-const request = require('request');
+const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-  const url =
-    'https://api.darksky.net/forecast/8f3ff6b6422c0856551f0f6c09ca507b/' + latitude + ',' + longitude;
+  const url = 'https://api.darksky.net/forecast/8f3ff6b6422c0856551f0f6c09ca507b/' + latitude + ',' + longitude
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
-      callback('Unable to connect to weather service!', undefined);
+      callback('Unable to connect to weather service!', undefined)
     } else if (body.error) {
-      callback('Unable to find location', undefined);
+      callback('Unable to find location', undefined)
     } else {
-      callback(undefined,
-        body.daily.data[0].summary +
-        ' It is currently ' +
-        body.currently.temperature +
-        ' degrees out. There is a ' +
-        body.currently.precipProbability +
-        '% chance of rain.'
-      );
+      callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.');
     }
   });
 };
 
-module.exports = forecast;
+module.exports = forecast
